88b17eec9ea397ff0e72712787a7a68440066d02
rafifi7 <79894190+rafifi7@users.noreply.github.com>
2023-12-05 00:02:29 -0500

    Merged mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,12 +3,25 @@
 
 #include <QMainWindow>
 #include <QTextStream>
-#include <QtCharts>
-
+#include <QDateTime>
+#include <QDate>
+#include <QTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+struct ProcessDetails {
+    QString name;
+    QString user;
+    QString state;
+    QString memory;
+    QString vmemory;
+    QString rmemory;
+    QString smemory;
+    QString cpuTime;
+    QDateTime startTime;
+};
 
 struct ProcessInfo {
     int pid;
@@ -20,52 +33,45 @@ struct ProcessInfo {
     // Other fields as needed
 };
 
-
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
     double hardDiskCheck(const QString &disk);
     void updateSystemInfo();
     void updateFileSystemInfo();
     void printAll(QString info, QTextStream &in);
     QString bytesToMebibytesString(unsigned long bytes);
-    void updateProcesses();
-    void createCpuBarChart();
-    void updateCpuBarChart();
-    void createRamSwapBarChart();
-    void updateRamSwapBarChart();
-    void createNetworkBarChart();
-    void updateNetworkBarChart();
-    void updateGraphs();
-    void updateNetworkResourceInfo();
-    void updateRamSwapResourceInfo();
-    std::vector<QLineSeries*> cpuLineSeriesVector; // Vector of QLineSeries pointers
-    std::vector<QLineSeries*> ramSwapLineSeriesVector; // Vector of QLineSeries pointers
-    std::vector<QLineSeries*> networkLineSeriesVector;
-    QChartView *cpuChartView;
-    QChartView *ramSwapChartView;
-    QChartView *networkChartView;
-    long recievedLast;
-    long uploadLast;
-//    void updateCPUResourceInfo();
-    void updateProcesses(bool showOnlyUserProcess, bool treeView);
+
+    void updateProcesses(bool showOnlyUserProcess);
+    void updateProcesses(bool showOnlyUserProcess, bool treeView); // Overloaded function
+    void processActions(const QPoint &pos);
     QString kbToMiB(const QString &memLine);
     QString getProcessUid(const QString &statusPath);
     QMap<int, ProcessInfo> readAllProcesses(bool showOnlyUserProcess);
     ProcessInfo readProcessInfo(const QString &statusPath);
     void buildProcessTree(const QMap<int, ProcessInfo> &processes);
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ProcessDetails getProcessDetails(int pid);
 
 private slots:
-    void updateCPUResourceInfo(); // Declaration for the CPU info update function
+    void updateCPUResourceInfo();
     void onProcessFilterChanged(int index);
     void pushButton_clicked();
+    void stopProcess(int pid);
+    void continueProcess(int pid);
+    void killProcess(int pid);
+    void listMapsProcess(int pid);
+    void showMemoryMapsDialog(const QStringList &lines);
+    void listFilesProcess(int pid);
+    void propertiesProcess(int pid);
 
 private:
     Ui::MainWindow *ui;
-    QTimer *graphInfoTimer; // QTimer object to trigger updates
+    QTimer *cpuInfoTimer;
 };
+
 #endif // MAINWINDOW_H
